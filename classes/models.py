db5c83c6d16b79b7c6fa8797a2eb9fd9b0356347
SergeyOlkin <4567310Ajiger@gmail.com>
2023-11-04 15:58:34 +0300

    Added changes

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from users.models import User
@@ -12,6 +13,8 @@ class Course(models.Model):
 
     lessons = models.ManyToManyField('Lesson', verbose_name='урок')
 
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
+
     def __str__(self):
         return f'{self.title}'
 
@@ -25,6 +28,8 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to="previews/", verbose_name="превью", **NULLABLE)
     video_link = models.URLField(verbose_name="ссылка на видео", **NULLABLE)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
---
+++ classes/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.owner == request.user
+
+
+class IsModeratorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user.groups.filter(name='Moderators').exists()
